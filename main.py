b09953262f09f417ccdf0fe68fb3013ff97ea950
VivaceJamiel <jimpoy2@gmail.com>
2021-07-22 00:53:01 -0500

    able to search for cves with keywords in them

--- main.py
+++ main.py
@@ -2,19 +2,43 @@
 import json
 
 # Get user input for which json file to open
-file = input("Enter the JSON filename\n")
-print('Opening ' + file + '...\n')
-
-# Open JSON File
-f = open(file,"r",encoding='utf-8')
-
+while True:
+    file = input("Enter the JSON filename\n")
+    print('\nOpening ' + file + '...\n')
+    try:
+        f = open(file,"r",encoding='utf-8')
+        break
+    except IOError:
+        input("Error in file opening, press Enter to enter filename again\n")
 
 # Returns JSON object as dictionary
 data = json.load(f)
+cves = data["CVE_Items"]
+
+# 'i' here is each cve key value
+# description values is this path "cves[ind]['cve']['description']['description_data'][0]['value']"
+ind = 0
+count = 0
+
+substrings = ['XSS', 'xss', 'cross site scripting', 'cross-site scripting']
+
+for i in cves:
+    description = cves[ind]['cve']['description']['description_data'][0]['value']
+    if any(word in description for word in substrings):
+        print(cves[ind]['cve']['CVE_data_meta']['ID'])
+        print(description)
+        count = count + 1
+        print('\n')
+    ind = ind + 1
+
+print("There are " + str(count) + " description(s) with xss in them")
 
 # Prints the JSON list
-for i in data["CVE_Items"]:
-    print(i + "\n")
+# searching = input("Enter data parameter you want to search for\n")
+# if searching in cves:
+#     print(cves[searching])
+# else:
+#     print("Parameter is not present")
 
 # Close file
 f.close
