0ae357dd608932fe9cd17909502aa4687f232297
VivaceJamiel <jimpoy2@gmail.com>
2021-07-25 16:08:07 -0500

    makes output file

--- main.py
+++ main.py
@@ -20,18 +20,27 @@ cves = data["CVE_Items"]
 ind = 0
 count = 0
 
+newJson = json.dumps(cves, indent = 4)
+# print(newJson)
+
+# Words to search for in description
 substrings = ['XSS', 'xss', 'cross site scripting', 'cross-site scripting']
 
+foundCVE = {}
+
 for i in cves:
+    CVE_id = cves[ind]['cve']['CVE_data_meta']['ID']
     description = cves[ind]['cve']['description']['description_data'][0]['value']
     if any(word in description for word in substrings):
-        print(cves[ind]['cve']['CVE_data_meta']['ID'])
-        print(description)
+        foundCVE[CVE_id] = description
         count = count + 1
         print('\n')
     ind = ind + 1
 
 print("There are " + str(count) + " description(s) with xss in them")
+
+with open('foundCVEs', 'w') as convert_file:
+    convert_file.write(json.dumps(foundCVE, indent=4))
 
 # Prints the JSON list
 # searching = input("Enter data parameter you want to search for\n")
